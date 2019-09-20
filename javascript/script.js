e72634599f1c1764b3867a4c2965ea857b4c75bf
Nikoloz <47790698+n1kalai@users.noreply.github.com>
2019-09-21 00:38:39 +0400

    ss

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,42 @@
 let $mainDiv = document.querySelector('.mainDiv');
 let $button = document.querySelectorAll('.nexter');
+let $images = document.querySelectorAll('.inner > IMG');
+
+
 
 //window.onload = fromLeft()
+if ($button[0].onclick) {
+    console.log('clicked')
+}
 
 for (var i = 0; i < $button.length; i++) {
 
-    console.log($button.length)
+
     $button[i].addEventListener('click', goLeft);
 
-    function goLeft() {
+
+
+    function goLeft(e) {
 
         let $article = this.previousElementSibling;
         $article.classList.toggle('left');
         this.classList.toggle('fromleft')
-        
+        if (this.classList.contains('fromleft')) {
+            this.innerHTML = '&#9776;'
+        } else {
+            this.innerHTML = '&#10006;'
+        }
+
+
     }
 }
-//$button.addEventListener('click', goLeft);
+
+
+for (var j = 0; j < $images.length; j++) {
+    $images[j].addEventListener('mouseover', function (e) {
+        if (e.target) {
+            e.preventDefault();
+            this.style.pointerEvents = 'auto';
+        }
+    })
+}
